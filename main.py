27fe70b4fc8b51a6de03d8d4f43283056c6fc696
juliusml <juliusnandreasson@gmail.com>
2021-08-12 11:57:28 +0200

    TestCommit4

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import gensim.downloader as gsd
 def model_import():
     """Imports all needed models."""
     # "load" only downloads it if it isn't already on the PC locally
-    en_model = gsd.load('en§§') 
-    se_model = gsd.load('se§§') 
+    #en_model = gsd.load('en§§') 
+    #se_model = gsd.load('se§§') 
 
-en_se_model = TWV(en_model, se_model).fit(train)
+#en_se_model = TWV(en_model, se_model).fit(train)
